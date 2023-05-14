d576170be39c3c242ee35c3fc031d1680ff36f7f
John Larson <35274970+waaade@users.noreply.github.com>
2023-05-13 19:23:03 -0600

    Handling login, and style

---
+++ 05-DollarsBank/js/login.js
@@ -0,0 +1,32 @@
+const url = "http://localhost:3000/users"
+
+if (localStorage.currentUser) {
+    document.getElementById("mainbody").innerHTML = "<p>You are logged in!</p>";
+} else {
+    document.getElementById("mainbody").innerHTML = "<h2>Login</h2>" + 
+        "<div id='loginform'>" +
+        "Username <input type='text' id='namefield'><br>" +
+        "Password <input type='password' id='password'><br><br>" +
+        "<button onclick='login()'>Submit</button></form>";
+}
+
+async function login() {
+    const query = url + "?username=" + document.getElementById("namefield").value + "&password=" + document.getElementById("password").value;
+    try {
+        const response = await
+        fetch(query);
+        const jsonData = await
+        response.json();
+        console.log(jsonData);
+        if (jsonData.length === 1) {
+            localStorage.setItem("currentUser", 1);
+            document.getElementById("mainbody").innerHTML = "<p>You are logged in!</p>";
+        }
+        else {
+            document.getElementById("error").innerHTML = "<p>Error Logging In. Try again</p>";
+        }
+    } catch (error) {
+        console.log(error);
+        document.getElementById("error").innerHTML = "<p>Error Logging In. Try again</p>";
+    }
+}
